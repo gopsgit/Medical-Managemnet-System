9891ad3368e38c713989da4a5f16fb1f57d219e3
gopsgit <gops.8605@gmail.com>
2021-07-15 10:49:56 +0530

    adding model into db

--- Hospital_Management_System/hospitalapp/models.py
+++ Hospital_Management_System/hospitalapp/models.py
@@ -13,10 +13,6 @@ class Company(models.Model):
     added_on = models.DateTimeField(auto_now_add=True)
 
 
-
-
-
-
 class Medicine(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
@@ -29,7 +25,7 @@ class Medicine(models.Model):
     shelf_no = models.CharField(max_length=255)
     msg_date = models.DateField()
     expire_date = models.DateField()
-    company_id = models.ForeigenKey(Company,on_delete=models.CASACDE)
+    company_id = models.ForeignKey(Company,on_delete=models.CASCADE)
     description = models.CharField(max_length=255)
     in_stock_total = models.IntegerField()
     qty_in_stripe = models.IntegerField()
@@ -37,7 +33,7 @@ class Medicine(models.Model):
 
 class MedicalDetails(models.Model):
     id = models.AutoField(primary_key = True)
-    medicine_id  = models.ForeigenKey(Medicine,on_delete= models.CASCADE)
+    medicine_id  = models.ForeignKey(Medicine,on_delete= models.CASCADE)
     salt_name = models.CharField(max_length=255)
     salt_qty = models.CharField(max_length=255)
     salt_qty_type = models.CharField(max_length=255)
@@ -58,6 +54,66 @@ class Customer(models.Model):
     address = models.CharField(max_length=255)
     contact = models.CharField(max_length=255)
     added_on = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class Bill(models.Model):
+    id = models.AutoField(primary_key=True)
+    customer_id = models.ForeignKey(Customer,on_delete = models.CASCADE)
+    added_on = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+    
+class EmployeeSalary(models.Model):
+    id = models.AutoField(primary_key=True)
+    employee_id = models.ForeignKey(Employee,on_delete = models.CASCADE)
+    salary_date = models.DateField()
+    salary_amount = models.CharField(max_length=255)
+    added_on = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class BillDetails(models.Model):
+    id = models.AutoField(primary_key=True)
+    bill_id = models.ForeignKey(Bill,on_delete = models.CASCADE)
+    medicine_id = models.ForeignKey(Medicine,on_delete = models.CASCADE)
+    qyt = models.IntegerField()
+    added_on = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class CustomerRequest(models.Model):
+    id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    medicine_details = models.CharField(max_length=255)
+    status =  models.BooleanField (default=False)
+    added_on = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class CompanyAccount(models.Model):
+    choices  = ((1,'Debit'),(2,'Credit'))
+
+    id = models.AutoField(primary_key=True)
+    company_id = models.ForeignKey(Company,on_delete=models.CASCADE)
+    trasaction_type =models.CharField(choices=choices,max_length=255)
+    transaction_amt = models.CharField(max_length=255)
+    transaction_date = models.DateField()
+    payment_mode  = models.CharField(max_length=255)
+    added_on = models.DateTimeField()
+    objects = models.Manager()
+
+class CompanyBank(models.Model):
+    id = models.AutoField(primary_key=True)
+    bank_account_no = models.CharField(max_length=255)
+    ifsc = models.CharField(max_length=255)
+    company_id = models.ForeignKey(Company,on_delete = models.CASCADE)
+    added_on = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class EmployeeBank(models.Model):
+    id = models.AutoField(primary_key=True)
+    bank_account_no = models.CharField(max_length=255)
+    ifsc = models.CharField(max_length=255)
+    employee_id = models.ForeignKey(Employee,on_delete = models.CASCADE)
+    added_on = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
 
 
